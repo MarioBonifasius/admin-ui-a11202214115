61bf4986cf8064c44ac03e4a86884e815db75093
MarioBonifasius <112919371+MarioBonifasius@users.noreply.github.com>
2025-11-04 11:28:57 +0700

    Add files via upload

---
+++ src/Exercise.jsx
@@ -0,0 +1,44 @@
+import React, { use, useEffect, useState } from "react";
+import UserCard from "./UserCard";
+import { getUsers } from "./Services";
+import PostCard from "./PostCard";
+import postsData from "../postsData";
+
+function Exercise() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUsers();
+        setUsers(data);
+      } catch (error) {
+        console.error("[Component] Gagal menampilkan data:", error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 p-6">
+        <h1 className="text-3xl font-bold text-center mb-6 text-special-red">
+          Post Cards
+        </h1>
+        <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-6 max-w-8xl mx-auto">
+          {postsData.map((post) => (
+            <PostCard 
+              key={post.id} 
+              id={post.id}
+              userId={post.userId}
+              title={post.title}
+              body={post.body}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Exercise;
