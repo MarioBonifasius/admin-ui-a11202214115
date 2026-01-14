46775103539b9129d1197226fe09ff1524068955
MarioBonifasius <112919371+MarioBonifasius@users.noreply.github.com>
2026-01-14 23:43:00 +0700

    uas

--- src/App.jsx
+++ src/App.jsx
@@ -14,74 +14,63 @@ import { useContext } from "react";
 import { AuthContext } from "./context/authContext.jsx";
 
 function App() {
-  const courses = [
+  const { user } = useContext(AuthContext);
+
+  const RequireAuth = ({ children }) => {
+    return user ? children : <Navigate to="/login" />;
+  };
+
+  const NotRequireAuth = ({ children }) => {
+    return !user ? children : <Navigate to="/" />;
+  };
+
+  const myRouter = createBrowserRouter([
     {
-      title: "System Administration and IT Infrastructure Services",
+      path: "/",
+      element: (
+        <RequireAuth>
+          <DashboardPage />
+        </RequireAuth>
+      ),
+      errorElement: <ErrorPage />,
     },
     {
-      title: "Operating Systems Becoming a Power User",
+      path: "/login",
+      element: (
+        <NotRequireAuth>
+          <SignInPage />
+        </NotRequireAuth>
+      ),
     },
     {
-      title: "The Bits and Bytes of Computer Networking",
+      path: "/register",
+      element: (
+        <NotRequireAuth>
+          <SignUpPage />
+        </NotRequireAuth>
+      ),
     },
     {
-      title: "Technical Support Fundamentals",
+      path: "/balance",
+      element: (
+        <RequireAuth>
+          <BalancePage />
+        </RequireAuth>
+      ),
     },
     {
-      title: "How to Succeed at: Writing Applications",
+      path: "/expense",
+      element: (
+        <RequireAuth>
+          <ExpensePage />
+        </RequireAuth>
+      ),
     },
-    {
-      title: "Medicine Administration for Carers",
-    },
-  ];
+  ]);
 
   return (
     <>
-      <div className="p-6 grid grid-cols-1 gap-6">
-        {courses.map((course, index) => (
-          <div key={index} className="shadow rounded-lg overflow-hidden">
-            {/* Image */}
-            <img
-              src="https://www.placehold.co/200x400"
-              alt={course.title}
-              className="w-full h-80 object-cover"
-            />
-
-            {/* Content */}
-            <div className="p-4 bg-red-100">
-              <h3 className="text-lg font-semibold mb-4">{course.title}</h3>
-
-              							<div className="bg-red-50 p-2 rounded-lg">
-                <div className="text-sm text-gray-500 mt-2">
-                  <span>👥 123 users</span>
-                  <span>⏱ 60</span>
-                </div>
-
-                {/* Author */}
-                <div className="mt-3 flex items-center gap-2">
-                  <img
-                    src="https://www.placehold.co/50x50"
-                    alt="Author's Avatar"
-                    className="w-12 h-12 rounded-full object-cover"
-                  />
-                  <div>
-                    <p className="text-sm font-medium">Author's Name</p>
-                    <p className="text-xs text-gray-500">Designer</p>
-                  </div>
-                </div>
-              </div>
-
-              {/* Footer */}
-              <div className="mt-4">
-                <div className="bg-white py-2 px-4 rounded">$123</div>
-                <button className="bg-red-700 text-white px-4 py-2 rounded text-sm">
-                  Get Started
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <RouterProvider router={myRouter} />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ThemeContextProvider } from "./context/themeContext";
+import { AuthContextProvider } from "./context/authContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthContextProvider>
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
+    </AuthContextProvider>
+  </StrictMode>
+);
